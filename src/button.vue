7613cb237208f7f42a27bd02086e9fb60cad2dd4
李承斌 <15031617367@163.com>
2020-11-12 14:38:52 +0800

    加入parcel,将button组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button>按钮</button>
+</template>
+
+<sript>
+  export default{
+
+  }
+</sript>
+
+<style lang="scss">
+  .g-button{
+    font-size:var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius:var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover{
+      border-color: var(--border-color-hover);
+    }
+    &:active{
+      background: var(--button-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+
+
+</style>
