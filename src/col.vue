616003f974ebe357fdad7beb31fbd22eeb20a27b
李承斌 <15031617367@163.com>
2020-11-20 13:41:23 +0800

    row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export  default {
+    name:"col",
+    props:{
+      span:{
+        type:[Number,String]
+      }
+    },
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .col{
+    height: 100px;
+    width: 50%;
+    background: gray;
+    border: 1px solid red;
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n /24) * 100%;
+      }
+    }
+  }
+
+
+
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name:"row",
+    data(){
+      return{
+
+      }
+    },
+    methods:{
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+.row{
+  display: flex;
+}
+</style>
