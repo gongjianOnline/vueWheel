5193a21811467a6bb7683726b71ee72512264b5c
李承斌 <15031617367@163.com>
2020-11-20 14:31:35 +0800

    栅格化布局(冗余版)

--- src/col.vue
+++ src/col.vue
@@ -1,19 +1,26 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col"
+       :class="[span &&`col-${span}`,offset && `offset-${offset}`]"
+        :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div style="border:1px solid green;height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
   export  default {
-    name:"col",
+    name:"g-col",
     props:{
       span:{
+        type:[Number,String]
+      },
+      offset:{
         type:[Number,String]
       }
     },
     data(){
       return{
-
+        gutter:0
       }
     },
     methods:{
@@ -23,16 +30,20 @@
 </script>
 <style lang="scss" scoped>
   .col{
-    height: 100px;
     width: 50%;
-    background: gray;
-    border: 1px solid red;
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n /24) * 100%;
       }
     }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-right: ($n /24) * 100%;
+      }
+    }
+
   }
 
 
