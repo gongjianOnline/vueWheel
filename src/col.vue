9d9aa1289462fe5cee795926b881e73951d93b86
前端小垃圾 <15031617367@163.com>
2020-11-21 12:05:21 +0800

    完成了gutter的基本功能(未优化)

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col"
+       :class="[`col-${span}`,offset && `offset-${offset}`]"
+        :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div style="border: 1px solid green;height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,11 +13,14 @@
     props:{
       span:{
         type:[Number,String]
+      },
+      offset:{
+        type:[Number,String]
       }
     },
     data(){
       return{
-
+        gutter:0
       }
     },
     methods:{
@@ -23,14 +30,20 @@
 </script>
 <style lang="scss" scoped>
   .col{
-    height: 100px;
+
     width: 50%;
-    background: gray;
-    border: 1px solid red;
+    //border: 1px solid red;
+    //padding: 0 10px;
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n /24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n /24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="row">
+  <div class="row"
+       :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 <script>
   export default {
     name:"g-row",
+    props:{
+      gutter:{
+        type:[Number,String],
+      }
+    },
     data(){
       return{
 
       }
+    },
+    created() {
+    },
+    mounted() {
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter;
+      })
     },
     methods:{
 
