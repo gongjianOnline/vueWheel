41a40c7a62c357d41c42fc5169f81b0dce804a8d
前端小垃圾 <15031617367@163.com>
2020-11-29 19:48:18 +0800

    加入了disabled功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -20,7 +20,8 @@ export  default {
   computed:{
     classes(){
       return {
-        active:this.active
+        active:this.active,
+        disabled: this.disabled
       }
     }
   },
@@ -30,7 +31,8 @@ export  default {
     })
   },
   methods:{
-    xxx(){
+    onClick(){
+      if(this.disabled){return}
       this.eventBus.$emit("update:selected",this.name,this)
     }
   },
@@ -43,6 +45,7 @@ export  default {
 </script>
 <style lang="scss" scoped>
 $blue:blue;
+$disabled-text-color:grey;
 .tabs-item{
   flex-shrink: 0;
   padding: 0 2em;
@@ -53,6 +56,10 @@ $blue:blue;
   &.active{
     color:$blue;
     font-weight: bold;
+  }
+  &.disabled{
+    color:$disabled-text-color;
+    cursor:no-drop
   }
 }
 </style>
