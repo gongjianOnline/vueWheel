8cc1bcd7297b2f943ceebb415a6bcf5bb7f38a0e
李承斌 <15031617367@163.com>
2020-12-10 15:20:57 +0800

    \创建了popover组件

---
+++ src/popover.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper" @click="xxx">
+    <slot name="content" v-if="visible"></slot>
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name:'g-popover',
+    data(){
+      return{
+        visible:false
+      }
+    },
+    props:{
+
+    },
+    created() {
+    },
+    mounted() {
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    methods:{
+      xxx(){
+        console.log("1")
+        console.log(this.visible)
+        this.visible = !this.visible
+      }
+    },
+    destroyed() {
+    }
+
+  }
+</script>
+<style lang="scss" scoped>
+  .wrapper{
+    display: inline-block;
+    vertical-align: top;
+  }
+
+</style>
