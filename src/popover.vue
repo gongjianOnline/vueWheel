f8b08ef03c488de7c62e932e8bbd2d046e264c93
李承斌 <15031617367@163.com>
2020-12-11 16:39:12 +0800

    重构popover组件

--- src/popover.vue
+++ src/popover.vue
@@ -1,78 +1,67 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
       <slot></slot>
     </span>
-
   </div>
 </template>
+
 <script>
   export default {
-    name:'g-popover',
-    data(){
-      return{
-        visible:false
-      }
+    name: "g-popover",
+    data () {
+      return {visible: false}
     },
-    props:{
-
-    },
-    created() {
-    },
-    mounted() {
-
-    },
-
-    computed:{
-
-    },
-    watch:{
-
-    },
-    methods:{
-      xxx(){
-        this.visible = !this.visible;
-        console.log("切换了visible")
-        if(this.visible === true){
-          this.$nextTick(()=>{
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = left+ window.scrollX + "px";
-            this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
-            console.log("新增 document click 监听器")
-            let eventHandler = ()=>{
-              console.log("点击body就关闭了popover")
-              this.visible = false;
-              console.log("删除监听器")
-              document.removeEventListener("click",eventHandler)
-            }
-            document.addEventListener('click',eventHandler)
-          })
+    methods: {
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true;
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
       }
-
-
-
-    },
-    destroyed() {
     }
-
   }
 </script>
-<style lang="scss" scoped>
-  .popover{
+
+<style scoped lang="scss">
+  .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
   }
-  .content-wrapper{
+  .content-wrapper {
     position: absolute;
     border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
     transform: translateY(-100%);
   }
-
 </style>
