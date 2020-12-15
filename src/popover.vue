6631e5247d36aafc425deb9ba3fa5b481fe68c70
李承斌 <15031617367@163.com>
2020-12-15 15:59:10 +0800

    popover功能基本实现

--- src/popover.vue
+++ src/popover.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div ref="contentWrapper"
          class="content-wrapper"
          v-if="visible"
         :class="{[`position-${position}`]:true}">
-      <slot name="content" ></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block">
       <slot></slot>
@@ -16,7 +16,10 @@
   export default {
     name: "g-popover",
     data () {
-      return {visible: false}
+      return {
+        visible: false,
+
+      }
     },
     props:{
       position:{
@@ -25,7 +28,46 @@
         validator(value){
           return ['top','bottom','left','right'].indexOf(value) >= 0;
         }
+      },
+      trigger:{
+        type:String,
+        default:"click",
+        validator(value){
+          return ['click','hover'].indexOf(value) >=0
+        }
       }
+    },
+    mounted() {
+      if(this.trigger === 'click'){
+        this.$refs.popover.addEventListener('click',this.onClick)
+      }else{
+        this.$refs.popover.addEventListener('mouseenter',this.open)
+        this.$refs.popover.addEventListener('mouseleave',this.close)
+      }
+    },
+    destroyed() {
+      if(this.trigger === 'click'){
+        this.$refs.popover.removeEventListener('click',this.onClick)
+      }else{
+        this.$refs.popover.removeEventListener('mouseenter',this.open)
+        this.$refs.popover.removeEventListener('mouseleave',this.close)
+      }
+    },
+    computed:{
+      openEvent(){
+        if(this.trigger === "click"){
+          return 'click'
+        }else{
+          return 'mouseenter'
+        }
+      },
+      closeEvent(){
+        if(this.trigger === "click"){
+          return 'click'
+        }else{
+          return 'mouseleave'
+        }
+      },
     },
     methods: {
       positionContent () {
@@ -120,10 +162,12 @@
       &::before{
         border-top-color: black;
         top:100%;
+        border-bottom: none;
       }
       &::after{
         border-top-color: white;
         top:calc(100% - 2px);
+        border-bottom: none;
       }
     }
     &.position-bottom{
@@ -131,6 +175,7 @@
       filter: drop-shadow(0 -1px 1px rgba(0, 0, 0, 0.5));
       &::before,&::after{
         left: 10px;
+        border-top: none;
       }
       &::before{
         border-bottom-color: black;
@@ -147,7 +192,8 @@
       &::before,&::after{
         top: 50%;
         transform: translateY(-50%);
-        left: 10px;
+        left: 100px;
+        border-right: none;
       }
       &::before{
         border-left-color: black;
@@ -164,6 +210,7 @@
       &::before, &::after {
         transform: translateY(-50%);
         top: 50%;
+        border-left: none;
       }
       &::before {
         border-right-color: black;
