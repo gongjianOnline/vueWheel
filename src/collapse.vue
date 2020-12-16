bf4512955f5a20dc60d4035a6b9fe1b27bda16b3
李承斌 <15031617367@163.com>
2020-12-16 09:49:57 +0800

    collapse 基本样式和基本功能完成

---
+++ src/collapse.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name:"g-collapse",
+    data(){
+      return{
+
+      }
+    },
+    props:{
+
+    },
+    computed:{
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods:{
+
+    },
+    destroyed() {
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  $grey:#ddd;
+  $border-radius:4px;
+  .collapse{
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+    border-bottom: none;
+  }
+</style>
