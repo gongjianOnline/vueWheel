3f844bf690ab555abad2429c811d0bd88caa260c
李承斌 <15031617367@163.com>
2020-12-18 09:26:03 +0800

    创建官网

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -2,6 +2,11 @@
   <div>
     <g-button class="xxx">默认按钮</g-button>
     <g-button icon="i-settings" class="xxx">设置</g-button>
+    <g-button :loading="true" class="xxx">设置</g-button>
+    <g-button disabled class="xxx">设置</g-button>
+    <pre>
+      <code>{{content}}</code>
+    </pre>
   </div>
 </template>
 <script>
@@ -12,7 +17,12 @@
     },
     data () {
       return{
-        test:'11'
+        content:`
+          <g-button>默认按钮</g-button>
+          <g-button icon="i-settings">设置</g-button>
+          <g-button :loading="true">设置</g-button>
+          <g-button disabled>设置</g-button>
+        `
       }
     },
 
